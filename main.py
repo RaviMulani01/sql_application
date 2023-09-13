104c24d87c44f816578dde6ca6a6bb69d2ea486c
Ravi Mulani <ravimulani101@gmail.com>
2023-09-13 17:03:11 -0400

    Add patch request to update the student info

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_students():
     except Exception as e:
         return jsonify({"error": str(e)})
     
-# Create get endpoint for retrive all student Info
+# Create post endpoint for add student 
 #URI : POST: http://127.0.0.1:5000/addstudents    
 @app.route('/addstudents', methods=['POST'])
 def add_student():
@@ -75,6 +75,45 @@ def add_student():
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
+# Create PATCH endpoint for add student 
+#URI : PATCH: http://127.0.0.1:5000//api/update/studentidNumber(1,2,3,etc..)       
+@app.route('/api/update/<int:id>', methods=['PATCH'])
+def update_data(id):
+    try:
+           # Establish a database connection
+        connection = Connection.getConn()
+
+        # Create a cursor to interact with the database
+        cursor = connection.cursor()
+        data = request.json  # Assuming the JSON data contains keys 'studentid', 'name', 'phonenumber', 'city'.
+
+        # Get data from the request
+        student_id = data['studentid']
+        name = data['name']
+        phone_number = data['phonenumber']
+        city = data['city']
+
+        # Execute an SQL query to update the student's information
+        update_query = "UPDATE student_info SET studentid=%s, name=%s, phonenumber=%s, city=%s WHERE studentid=%s"
+        cursor.execute(update_query, (student_id, name, phone_number, city, id))
+
+        # Check how many rows were affected by the update
+        rows_affected = cursor.rowcount
+
+        connection.commit()
+
+        # Close cursor and connection
+        cursor.close()
+        Connection.closeConn(connection)
+
+        if rows_affected == 0:
+            return 'No matching student found for the specified studentid', 404
+        else:
+            return 'Data updated successfully'
+    
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     # make debug true to see the log details while app is running
